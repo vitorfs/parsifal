48b069b7d2dab9419591f4f7d9c6588aa19d1cf7
Vitor Freitas <vitorfs@gmail.com>
2013-08-05 19:31:34 -0300

    refactoring style and html to use django template

--- parsifal/core/views.py
+++ parsifal/core/views.py
@@ -20,7 +20,7 @@ def signin(request):
                 if 'next' in request.GET:
                     return redirect(request.GET['next'])
                 else:
-                    return redirect('/')
+                    return redirect('/reviews/')
             else:
                 messages.add_message(request, messages.ERROR, 'Your account is desactivated.')
                 context = RequestContext(request)
