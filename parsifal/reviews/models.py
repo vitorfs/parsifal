a036097427a6166338101faec6b4793250290f04
Vitor Freitas <vitorfs@gmail.com>
2013-08-08 15:42:58 -0300

    creating review model

--- parsifal/reviews/models.py
+++ parsifal/reviews/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Review(models.Model):
+  short_name = models.CharField(max_length=50)
+  title = models.CharField(max_length=200)
+  description = models.CharField(max_length=500)
+
+  def __unicode__(self):
+      return self.short_name
+
+  class Meta:
+      verbose_name = "Review"
+      verbose_name_plural = "Reviews"
