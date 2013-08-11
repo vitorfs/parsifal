ec960d63e9ae52c7b453f679cc658b323f0168aa
Vitor Freitas <vitorfs@gmail.com>
2013-08-11 18:05:46 -0300

    refactoring edit profile page, creating a more beautiful secondary menu. changed the home page

--- parsifal/core/views.py
+++ parsifal/core/views.py
@@ -3,11 +3,14 @@ from django.contrib import messages
 from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.template import RequestContext
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login as django_login, logout as django_logout
+from django.contrib.auth import authenticate, login, logout
 
 def home(request):
   context = RequestContext(request)
-  return render_to_response('core/home.html', context)
+  if request.user.is_authenticated():
+    return render_to_response('core/home.html', context)
+  else:
+    return render_to_response('core/cover.html', context)
 
 def signin(request):
   if request.method == 'POST':
@@ -16,11 +19,11 @@ def signin(request):
     user = authenticate(username=username, password=password)
     if user is not None:
       if user.is_active:
-        django_login(request, user)
+        login(request, user)
         if 'next' in request.GET:
           return redirect(request.GET['next'])
         else:
-          return redirect('/' + username + '/')
+          return redirect('/')
       else:
         messages.add_message(request, messages.ERROR, 'Your account is desactivated.')
         context = RequestContext(request)
@@ -34,5 +37,6 @@ def signin(request):
     return render_to_response('core/signin.html', context)
 
 def signout(request):
-  django_logout(request)
+  logout(request)
+  messages.add_message(request, messages.SUCCESS, 'You have successfully signed out.')
   return redirect('/signin/')
