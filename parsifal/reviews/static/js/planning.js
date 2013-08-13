8c0f7dc59947e613ea61acf9b04059fd8f86a057
Vitor Freitas <vitorfs@gmail.com>
2013-08-13 00:13:05 -0300

    creating a beautiful modal for the app. first usage: suggested sources

--- parsifal/reviews/static/js/planning.js
+++ parsifal/reviews/static/js/planning.js
@@ -1,5 +1,10 @@
 $(function () {
   $("#btn-add-source").click(function () {
-    $("#tbl-sources tbody").append("<tr><td>Teste</td><td>URL</td><td>edit remove</td></tr>");
+    $("#tbl-sources tbody").prepend('<tr><td><input type="text" id="name"></td><td><input type="text" id="url"></td><td><button type="button" class="btn btn-success btn-small">save</button> <button type="button" class="btn btn-warning btn-small">cancel</button></tr>');
+    $("#tbl-sources tbody tr:first-child td:first-child input").focus();
+  });  
+
+  $("#btn-suggested-sources").click(function () {
+    $("#modal-suggested-sources").slideDown();
   });
 });
