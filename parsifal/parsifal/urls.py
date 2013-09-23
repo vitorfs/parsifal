0ffeccec00b2dbab07e08a74418e2ed2ddee94c5
Vitor Freitas <vitorfs@gmail.com>
2013-09-23 00:43:23 -0300

    adding save route

--- parsifal/parsifal/urls.py
+++ parsifal/parsifal/urls.py
@@ -50,5 +50,6 @@ urlpatterns = patterns('',
     (r'^(?P<username>[\w-]+)/(?P<review_name>[\w-]+)/conducting/$', 'reviews.views.conducting'),
     (r'^(?P<username>[\w-]+)/(?P<review_name>[\w-]+)/reporting/$', 'reviews.views.reporting'),
     (r'^(?P<username>[\w-]+)/(?P<review_name>[\w-]+)/settings/$', 'reviews.views.settings'),
+    (r'^(?P<username>[\w-]+)/(?P<review_name>[\w-]+)/settings/save/$', 'reviews.views.settings'),
     (r'^(?P<username>[\w-]+)/$', 'reviews.views.reviews'),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
