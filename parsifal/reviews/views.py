c107b4ffd8fb5e98de6c3b6928e7a031e7a1c840
Vitor Freitas <vitorfs@gmail.com>
2013-09-26 23:31:18 -0300

    added view for save setting

--- parsifal/reviews/views.py
+++ parsifal/reviews/views.py
@@ -652,3 +652,8 @@ def settings(request, username, review_name):
     review = Review.objects.get(name=review_name, author__username=username)
     context = RequestContext(request, {'review': review,})
     return render_to_response('reviews/settings.html', context)
+
+@login_required
+def save_settings(request, username, review_name):
+    context = RequestContext(request)
+    return render_to_response('reviews/settings.html', context)
