f4d43b4a050ae58e454e89a946a7f15fa13a969d
Vitor Freitas <vitorfs@gmail.com>
2014-02-11 13:07:45 -0200

    changing media_root

--- parsifal/settings.py
+++ parsifal/settings.py
@@ -31,7 +31,7 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-MEDIA_ROOT = PROJECT_DIR.child('media')
+MEDIA_ROOT = PROJECT_DIR.parent.child('media')
 MEDIA_URL = '/media/'
 
 STATIC_ROOT = PROJECT_DIR.child('static')
