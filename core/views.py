8e27e03f18c93b69e843ebbc204f2cf0b91c6d32
Vitor Freitas <vitorfs@gmail.com>
2014-02-18 17:00:18 -0300

    updating forbidden names

--- core/views.py
+++ core/views.py
@@ -25,7 +25,12 @@ def signup(request):
 
         forbidden_usernames = ['admin', 'settings', 'news', 'about', 'help', 'signin', 'signup', 
             'signout', 'terms', 'privacy', 'cookie', 'new', 'login', 'logout', 'administrator', 
-            'join', 'account', 'username', 'www', 'root']
+            'join', 'account', 'username', 'root', 'blog', 'user', 'users', 'billing', 'subscribe',
+            'reviews', 'review', 'blog', 'blogs', 'edit', 'mail', 'email', 'home', 'job', 'jobs', 
+            'contribute', 'newsletter', 'shop', 'profile', 'register', 'auth', 'authentication',
+            'campaign', 'config', 'delete', 'remove', 'forum', 'forums', 'download', 'downloads', 
+            'contact', 'blogs', 'feed', 'faq', 'intranet', 'log', 'registration', 'search', 
+            'explore', 'rss', 'support', 'status', 'static', 'media', 'setting', 'css', 'js']
 
         username = request.POST['username']
         if not username:
