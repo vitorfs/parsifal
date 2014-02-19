a48afa9d1ead0fef56e279b760817e6d494db4d8
Vitor Freitas <vitorfs@gmail.com>
2014-02-19 01:09:43 -0300

    started using decouple, and cleaning settings.py

--- parsifal/settings.py
+++ parsifal/settings.py
@@ -1,19 +1,14 @@
 from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
 from unipath import Path
 import dj_database_url
+from decouple import config
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 PROJECT_DIR = Path(__file__).parent
 
-DEBUG = True 
+DEBUG = config('DEBUG', default=False, cast=bool) 
 TEMPLATE_DEBUG = DEBUG
-
-ADMINS = (
-    ('Vitor Freitas', 'vitor@freitas.com'),
-)
-
-MANAGERS = ADMINS
 
 DATABASES = {
     'default': dj_database_url.config(
@@ -25,8 +20,6 @@ ALLOWED_HOSTS = ['*']
 TIME_ZONE = 'America/Sao_Paulo'
 LANGUAGE_CODE = 'en-us'
 
-SITE_ID = 1
-
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
@@ -36,23 +29,11 @@ MEDIA_URL = '/media/'
 
 STATIC_ROOT = PROJECT_DIR.child('static')
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = (
     PROJECT_DIR.parent.child('static'),
 )
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
 
-SECRET_KEY = '(ssq$-7j1w5i_jf**z0=wih6^iqk5miq40wigyuckowk$-g5$z'
-
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-)
+SECRET_KEY = config('SECRET_KEY')
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
@@ -60,10 +41,11 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'parsifal.urls'
+
 WSGI_APPLICATION = 'parsifal.wsgi.application'
 
 TEMPLATE_DIRS = (
@@ -83,6 +65,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'south',
     'core',
+    'blog',
     'reviews',
     'reviews.planning',
     'reviews.conducting',
@@ -95,27 +78,3 @@ INSTALLED_APPS = (
 AUTH_PROFILE_MODULE = 'users.UserProfile'
 
 LOGIN_URL = '/signin/'
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
