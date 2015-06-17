d6129bbd6ee6d8ac8e896f3c33d1b9c4d624868d
Vitor Freitas <vitorfs@gmail.com>
2015-06-17 22:37:04 +0300

    working on dropbox integration

--- parsifal_settings/views.py
+++ parsifal_settings/views.py
@@ -4,6 +4,7 @@ import os
 from PIL import Image
 from mendeley.session import MendeleySession
 from oauthlib.oauth2 import TokenExpiredError
+from dropbox.client import DropboxClient, DropboxOAuth2Flow
 
 from django.core.urlresolvers import reverse as r
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
