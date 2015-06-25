7a78591517fe609eed2ac98468a3e083041f9021
Vitor Freitas <vitorfs@gmail.com>
2015-06-25 18:01:32 +0300

    Add library toggle document selection
    Also reduced the font-size of the "+ Add folder..." and "Connect to Dropbox..." buttons
    Change the active panel menu style class.

--- parsifal/library/static/js/library.js
+++ parsifal/library/static/js/library.js
@@ -1,13 +1,53 @@
 $(function () {
 
+  'use strict';
+
   $(".js-jump-page").popover({
     html: true,
     content: function () {
+      var last_page = $("#library-documents").attr("data-num-pages");
       var html = "<div><small><a href='?page=1'>First page</a></small></div>";
-      html += "<div><small><a href='?page={{ documents.paginator.num_pages }}'>Last page</a></small></div>";
+      html += "<div><small><a href='?page=" + last_page + "'>Last page</a></small></div>";
       return html;
     }
   });
+
+  $(".js-toggle-select-documents").click(function () {
+    var isChecked = $(".glyphicon", this).hasClass("glyphicon-check");
+    var btnSelectAll = $(this);
+    if (isChecked) {
+      $("#library-documents .js-document-checkbox").each(function () {
+        $(this).unselectDocument();
+      });
+      $(".glyphicon", btnSelectAll).removeClass().addClass("glyphicon glyphicon-unchecked");
+    }
+    else {
+      $("#library-documents .js-document-checkbox").each(function () {
+        $(this).selectDocument();
+      });
+      $(".glyphicon", btnSelectAll).removeClass().addClass("glyphicon glyphicon-check");
+    }
+  });
+
+  $.fn.selectDocument = function () {
+    var row = $(this).closest("tr");
+    var checkbox = $("input[type='checkbox']", this);
+    var icon = $(".glyphicon", this);
+
+    $(checkbox).prop("checked", true);
+    $(icon).removeClass().addClass("glyphicon glyphicon-check");
+    $(row).addClass("bg-warning");
+  };
+
+  $.fn.unselectDocument = function () {
+    var row = $(this).closest("tr");
+    var checkbox = $("input[type='checkbox']", this);
+    var icon = $(".glyphicon", this);
+
+    $(checkbox).prop("checked", false);
+    $(icon).removeClass().addClass("glyphicon glyphicon-unchecked");
+    $(row).removeClass("bg-warning");
+  };
 
   $(".js-document-checkbox").click(function () {
     var row = $(this).closest("tr");
@@ -16,14 +56,10 @@ $(function () {
     var isChecked = $(checkbox).is(":checked");
 
     if (isChecked) {
-      $(checkbox).prop("checked", false);
-      $(icon).removeClass().addClass("glyphicon glyphicon-unchecked");
-      $(row).removeClass("bg-warning");
+      $(this).unselectDocument();
     }
     else {
-      $(checkbox).prop("checked", true);
-      $(icon).removeClass().addClass("glyphicon glyphicon-check");
-      $(row).addClass("bg-warning");
+      $(this).selectDocument();
     }
 
   });
