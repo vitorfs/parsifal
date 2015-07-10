d79d7bee2b4ac17b5bb6743a032fa450b2b0246c
Vitor Freitas <vitorfs@gmail.com>
2015-07-11 00:52:34 +0300

    add admin for reviews

---
+++ reviews/admin.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+from django.contrib import admin
+
+from reviews.models import Review
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['name', 'title', 'description', 'author', 'create_date', 'last_update',]
+    search_fields = ['name', 'title', 'description']
+    fields = ['name', 'title', 'description', 'author', 'create_date', 'last_update', 'objective', 'sources', 'status', 'co_authors', 'quality_assessment_cutoff_score', 'population', 'intervention', 'comparison', 'outcome', 'context',]
+
+
+admin.site.register(Review, ReviewAdmin)
