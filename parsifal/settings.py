c7d6d361bea88a70b7158618669a6e21e4598a56
Vitor Freitas <vitorfs@gmail.com>
2015-08-01 15:47:28 +0300

    Add security middleware

--- parsifal/settings.py
+++ parsifal/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
 )
 
 ROOT_URLCONF = 'parsifal.urls'
