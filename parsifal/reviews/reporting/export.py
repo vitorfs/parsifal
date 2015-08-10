a555dfdb75905da22c45c96e0cd0bd3f169dc6dc
Vitor Freitas <vitorfs@gmail.com>
2015-08-10 12:37:20 +0300

    Update requirements.txt. Add full protocol to export

--- parsifal/reviews/reporting/export.py
+++ parsifal/reviews/reporting/export.py
@@ -27,6 +27,9 @@ def export_review_to_docx(review):
     if review.objective:
         document.add_paragraph(review.objective)
 
+    '''
+        PICOC
+    '''
     document.add_heading('1.1 PICOC', level=3)
 
     p = document.add_paragraph('', style='List Bullet')
@@ -49,8 +52,59 @@ def export_review_to_docx(review):
     p.add_run('Context: ').bold = True
     p.add_run(review.context)
 
+    '''
+        Research Questions
+    '''
     document.add_heading('1.2 Research Questions', level=3)
+
     for question in review.research_questions.all():
         document.add_paragraph(question.question, style='List Number')
+
+    '''
+        Keywords and Synonym
+    '''
+    document.add_heading('1.3 Keywords and Synonyms', level=3)
+
+    table = document.add_table(rows=1, cols=2)
+    hdr_cells = table.rows[0].cells
+    hdr_cells[0].text = 'Keyword'
+    hdr_cells[1].text = 'Synonyms'
+
+    for keyword in review.get_keywords():
+        row_cells = table.add_row().cells
+        row_cells[0].text = keyword.description
+        row_cells[1].text = ', '.join(keyword.synonyms.all().values_list('description', flat=True))
+
+    '''
+        Search String
+    '''
+    document.add_heading('1.4 Search String', level=3)
+    document.add_paragraph(review.get_generic_search_string().search_string)
+
+    '''
+        Sources
+    '''
+    document.add_heading('1.5 Sources', level=3)
+
+    for source in review.sources.all():
+        text = source.name
+        if source.url:
+            text = u'{0} ({1})'.format(source.name, source.url)
+        document.add_paragraph(text, style='List Bullet')
+
+    '''
+        Selection Criteria
+    '''
+    document.add_heading('1.6 Selection Criteria', level=3)
+
+    p = document.add_paragraph()
+    p.add_run('Inclusion Criteria:').bold = True
+    for criteria in review.get_inclusion_criterias():
+        document.add_paragraph(criteria.description, style='List Bullet')
+
+    p = document.add_paragraph()
+    p.add_run('Exclusion Criteria:').bold = True
+    for criteria in review.get_exclusion_criterias():
+        document.add_paragraph(criteria.description, style='List Bullet')
 
     return document
