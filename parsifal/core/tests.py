3dd0982493023b1de760304e9262d9c4f8139fb0
Vitor Freitas <vitorfs@gmail.com>
2015-08-18 15:03:53 +0300

    Update travis tests

--- parsifal/core/tests.py
+++ parsifal/core/tests.py
@@ -1,16 +1,10 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
+# coding: utf-8
 
 from django.test import TestCase
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class HomepageTest(TestCase):
+    def test_get(self):
+        response = self.client.get('/')
+        self.assertEqual(200, response.status_code)
+        self.assertTemplateUsed(response, 'core/cover.html')
