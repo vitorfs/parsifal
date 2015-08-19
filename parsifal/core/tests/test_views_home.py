9523515fc80ede2bf850bf2d8a29a5dfebe626ad
Vitor Freitas <vitorfs@gmail.com>
2015-08-19 12:20:25 +0300

    Add home test cases

--- parsifal/core/tests/test_views_home.py
+++ parsifal/core/tests/test_views_home.py
@@ -1,8 +1,10 @@
 # coding: utf-8
 
 from django.test import TestCase, Client
+from django.contrib.auth.models import User
 
-class HomeUnsignedUser(TestCase):
+
+class HomeUnauthenticatedUser(TestCase):
     def setUp(self):
         self.client = Client()
         self.response = self.client.get('/')
@@ -12,3 +14,17 @@ class HomeUnsignedUser(TestCase):
 
     def test_template(self):
         self.assertTemplateUsed(self.response, 'core/cover.html')
+
+
+class HomeAuthenticatedUser(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='john', password='s3cr3tp4ssw0rd', email='john@doe.com')
+        self.client = Client()
+        self.client.login(username='john', password='s3cr3tp4ssw0rd')
+        self.response = self.client.get('/')
+
+    def test_get(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_template(self):
+        self.assertTemplateUsed(self.response, 'core/home.html')
