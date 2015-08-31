6b2f2476364a35826a9293fea65c09ed05c9bdd3
Vitor Freitas <vitorfs@gmail.com>
2015-08-31 18:08:12 +0300

    add list filters

--- parsifal/reviews/admin.py
+++ parsifal/reviews/admin.py
@@ -8,6 +8,7 @@ from parsifal.reviews.models import Review
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ['name', 'title', 'description', 'author', 'create_date', 'last_update',]
     search_fields = ['name', 'title', 'description']
+    filter_horizontal = ('co_authors',)
     fields = ['name', 'title', 'description', 'author', 'objective', 'sources', 'status', 'co_authors', 'quality_assessment_cutoff_score', 'population', 'intervention', 'comparison', 'outcome', 'context',]
 
 
