e01b0f02725c115760500a2d4784affea52a868d
Vitor Freitas <vitorfs@gmail.com>
2021-08-28 23:03:48 -0300

    Add django 3 database migrations

--- parsifal/settings/tests.py
+++ parsifal/settings/tests.py
@@ -3,14 +3,3 @@
 from .base import *
 
 PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
-
-
-class DisableMigrations:
-    def __contains__(self, item):
-        return True
-
-    def __getitem__(self, item):
-        return None
-
-
-MIGRATION_MODULES = DisableMigrations()
