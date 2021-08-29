7db05ee8c471b2ff53d9c128c180b3647ecc9438
Vitor Freitas <vitorfs@gmail.com>
2021-08-28 23:20:30 -0300

    Improve blog database queries.

--- parsifal/blog/views.py
+++ parsifal/blog/views.py
@@ -4,12 +4,18 @@ from parsifal.blog.models import Entry
 
 
 def entries(request):
-    entries = Entry.objects.filter(status=Entry.PUBLISHED).order_by(
-        "-start_publication",
+    entries = (
+        Entry.objects.select_related("created_by__profile")
+        .filter(status=Entry.PUBLISHED)
+        .order_by(
+            "-start_publication",
+        )
     )
     return render(request, "blog/entries.html", {"entries": entries})
 
 
 def entry(request, slug):
-    blog_entry = get_object_or_404(Entry, slug=slug, status=Entry.PUBLISHED)
+    blog_entry = get_object_or_404(
+        Entry.objects.select_related("created_by__profile"), slug=slug, status=Entry.PUBLISHED
+    )
     return render(request, "blog/entry.html", {"entry": blog_entry})
