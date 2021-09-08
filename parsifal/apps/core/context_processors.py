922dc5ccb74d7a88ccfcc184c5719200e4c33646
Vitor Freitas <vitorfs@gmail.com>
2021-09-07 22:29:00 -0300

    Upgrade pillow. Add sentry integration.

--- parsifal/apps/core/context_processors.py
+++ parsifal/apps/core/context_processors.py
@@ -1,10 +1,15 @@
 from django.conf import settings as django_settings
 
+import parsifal
+
 
 def settings(request):
     return {
-        "environment": django_settings.PARSIFAL_ENVIRONMENT,
+        "parsifal_version": parsifal.__version__,
+        "parsifal_release": django_settings.PARSIFAL_RELEASE,
+        "parsifal_environment": django_settings.PARSIFAL_ENVIRONMENT,
         "recaptcha_enabled": django_settings.GOOGLE_RECAPTCHA_ENABLED,
         "recaptcha_site_key": django_settings.GOOGLE_RECAPTCHA_SITE_KEY,
         "google_analytics_ua": django_settings.GOOGLE_ANALYTICS_UA,
+        "sentry_dsn": django_settings.SENTRY_DSN,
     }
